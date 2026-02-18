cb748be3f70d5fff6027cf3688d33fa82d8714a0
Linc <lecluff@gmail.com>
2026-02-17 21:40:32 -0700

    Added OpenSSH and disabled password login after adding key

--- configuration.nix
+++ configuration.nix
@@ -68,6 +68,16 @@
     pulse.enable = true;
   };
 
+  services.openssh = {
+    enable = true;
+    ports = [ 2222 ];
+    settings = {
+      PasswordAuthentication = false;
+      AllowUsers = [ "linc" ];
+      PermitRootLogin = "no";
+    };
+  };
+
   # 1. Enable the Tailscale service
   services.tailscale.enable = true;
 
